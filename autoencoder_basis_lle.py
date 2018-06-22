c1fc6f4e58430116a5aa1d7a5c216a67e712c98b
Chunyu Wang <chunyu.wangdlut@gmail.com>
2018-06-22 03:17:09 -0400

    change name of autoencoder_lle.py to autoencoder_basis_lle.py

--- autoencoder_basis_lle.py
+++ autoencoder_basis_lle.py
@@ -45,18 +45,24 @@ class autoencoder(nn.Module):
         return s, y
 
 
-model = autoencoder([3, 200, 100, 50, 30, 2]).cuda()
+model = autoencoder([3, 500, 200, 100, 50, 30, 2]).cuda()
 criterion = mse_loss
-optimizer = torch.optim.Adam(model.parameters(), lr=1e-3, weight_decay=1e-5)
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-5)
 
 batch_size = 500
 data_path = './som_swiss_roll.h5'
 dataset = SwissRollDataset(data_path)
 dataloader = SwissRollDataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-num_epochs = 2000
+testdataset = SwissRollDataset(data_path)
+testdataloader = SwissRollDataLoader(
+    testdataset, batch_size=500, shuffle=False, add_basis=False)
+
+num_epochs = 40000
 for epoch in range(num_epochs):
     iteration = 0
+    epoch_recon_loss = 0
+    epoch_topol_loss = 0
     for data in dataloader:
         x = torch.tensor(data['data']).float()
         y = torch.tensor(data['label']).float()
@@ -66,65 +72,67 @@ for epoch in range(num_epochs):
         c = Variable(c).cuda()
         # ===================forward=====================
         hidden, output = model(x)
-
         data_hidden = Variable(
             hidden[:batch_size, :], requires_grad=True).cuda()
         basis_hidden = Variable(
             hidden[batch_size:, :], requires_grad=True).cuda()
-        torch.cuda.synchronize()
 
+        # ===================loss========================
+        torch.cuda.synchronize()
         loss_recon = criterion(output, y)
         loss_topol = criterion(torch.mm(c, basis_hidden), data_hidden)
-        loss = loss_recon + 10*loss_topol
-
+        loss = loss_recon + loss_topol
         torch.cuda.synchronize()
-        # ===================backward====================
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        epoch_recon_loss += loss_recon.item()
+        epoch_topol_loss += loss_topol.item()
         iteration += 1
 
     print('Epoch[{}/{}], loss:{:.4f}, loss:{:.4f}'.format(
-        epoch + 1, num_epochs, loss_recon.item(), loss_topol.item()))
+        epoch + 1, num_epochs, epoch_recon_loss / iteration,
+        epoch_topol_loss / iteration))
     dataloader.reset()
 
+    if np.mod(epoch, 100) == 0:
+        with torch.no_grad():
+            hiddens = []
+            colors = []
+            src_data = []
+            for data in testdataloader:
+                x = torch.tensor(data['data']).float()
+                x = Variable(x).cuda()
+                hidden, output = model(x)
+                hidden = hidden.cpu().numpy()
+                hiddens.append(hidden)
+                color = data['color']
+                colors.append(color)
+                #src_data.append(data['data'])
+                src_data.append(output.cpu().numpy())
+
+        hiddens = np.concatenate(hiddens, axis=0)
+        src_data = np.concatenate(src_data, axis=0)
+        colors = np.concatenate(colors, axis=0)
+
+        fig = plt.figure()
+        ax = fig.add_subplot(211, projection='3d')
+        ax.scatter(
+            src_data[:, 0],
+            src_data[:, 1],
+            src_data[:, 2],
+            c=colors,
+            cmap=plt.cm.Spectral)
+
+        ax.set_title("Original data")
+        ax = fig.add_subplot(212)
+        ax.scatter(hiddens[:, 0], hiddens[:, 1], c=colors, cmap=plt.cm.Spectral)
+        plt.axis('tight')
+        plt.xticks([]), plt.yticks([])
+        plt.title('Projected data')
+        #plt.show()
+        plt.savefig('{}.jpg'.format(epoch))
+        plt.close()
+        testdataloader.reset()
+
 torch.save(model.state_dict(), './autoencoder_lle.pth')
-
-dataset = SwissRollDataset(data_path)
-dataloader = SwissRollDataLoader(
-    dataset, batch_size=500, shuffle=False, add_basis=False)
-with torch.no_grad():
-    hiddens = []
-    colors = []
-    src_data = []
-    for data in dataloader:
-        x = torch.tensor(data['data']).float()
-        x = Variable(x).cuda()
-        hidden, output = model(x)
-        hidden = hidden.cpu().numpy()
-        hiddens.append(hidden)
-        color = data['color']
-        colors.append(color)
-        #src_data.append(data['data'])
-        src_data.append(output.cpu().numpy())
-
-hiddens = np.concatenate(hiddens, axis=0)
-src_data = np.concatenate(src_data, axis=0)
-colors = np.concatenate(colors, axis=0)
-
-fig = plt.figure()
-ax = fig.add_subplot(211, projection='3d')
-ax.scatter(
-    src_data[:, 0],
-    src_data[:, 1],
-    src_data[:, 2],
-    c=colors,
-    cmap=plt.cm.Spectral)
-
-ax.set_title("Original data")
-ax = fig.add_subplot(212)
-ax.scatter(hiddens[:, 0], hiddens[:, 1], c=colors, cmap=plt.cm.Spectral)
-plt.axis('tight')
-plt.xticks([]), plt.yticks([])
-plt.title('Projected data')
-plt.show()
